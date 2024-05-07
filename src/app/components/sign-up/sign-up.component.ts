9a3b16ee6b6304a39f713c256bccbe9412c6d715
Bhavin Kareliya <bhavin.kareliya2017@gmail.com>
2024-05-07 21:03:45 +0530

    Sign up page design and validation

---
+++ src/app/components/sign-up/sign-up.component.ts
@@ -0,0 +1,31 @@
+import { JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  imports: [ReactiveFormsModule, JsonPipe, RouterLink],
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.css'
+})
+export class SignUpComponent {
+  userForm: FormGroup
+  isValid: boolean = false
+  constructor() {
+    this.userForm = new FormGroup({
+      id: new FormControl(0),
+      firstname: new FormControl('', [Validators.required, Validators.maxLength(12)]),
+      lastname: new FormControl('', [Validators.required, Validators.maxLength(12)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+    });
+  }
+
+  SubmitEvent() {
+    this.isValid = this.userForm.invalid;
+    debugger;
+    const obj = this.userForm.value;
+  }
+}
