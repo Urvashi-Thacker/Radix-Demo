a54414c023d47474e617142c19d0f7685d9ba04c
Urvashi Thacker <urvashi.thacker@radixweb.local>
2024-05-15 18:42:46 +0530

    Grid Update

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { EmployeeComponent } from '../employee/employee.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [EmployeeComponent],
+  imports: [EmployeeComponent, HttpClientModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+ userArray : any[]=[]
+ 
+  constructor(private http : HttpClient){
+   this.GetAll()
+  }
+  GetAll(){
+   
+    this.http.get("https://localhost:7071/User/GetAll").subscribe((res : any)=>{
+      this.userArray = res
+    })
+  }
+  
 }
--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,14 +1,21 @@
 
+import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component, ViewEncapsulation  } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FileUploadModule } from 'primeng/fileupload';
 
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { Console } from 'console';
+import { copyFile } from 'fs';
+import path from 'path';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive,ReactiveFormsModule, HttpClientModule],
+  imports: [RouterLink, RouterLinkActive, ReactiveFormsModule, HttpClientModule, CommonModule, FileUploadModule, AvatarModule, AvatarGroupModule],
   templateUrl: './employee.component.html',
 
   styleUrl: './employee.component.css'
@@ -16,50 +23,80 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class EmployeeComponent {
 
-  employeeForm : FormGroup
-
-  constructor(private http: HttpClient){
-    this.employeeForm = new FormGroup({
-      
-      avatarUrl : new FormControl('',[Validators.required]),
-      firstName : new FormControl('',[Validators.required, Validators.maxLength(12)]),
-      lastName : new FormControl('',[Validators.required, Validators.maxLength(12)]),
-      email : new FormControl('',[Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
-      departmentId : new FormControl(parseInt,[Validators.required]),
-      dob : new FormControl('',[
-        Validators.required,Validators.pattern(/^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)$/
-      )
-      ]),
-      skillIds : new FormControl('',Validators.required),
-      shiftIds : new FormControl('',Validators.required,),
-      isActive : new FormControl('',Validators.required)
-      
-    })
-    
+  employeeForm: FormGroup
+  selectedFile: File | null = null;
+  copiedImageFile: File | null = null;
+  handleDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
   }
-  isActive : boolean=false
+
+  handleDrop(event: DragEvent) {
+    event.preventDefault();
+    if (event.dataTransfer) {
+      const file: File = event.dataTransfer.files[0];
+
+    }
+  }
+  onFileSelected(event: any) : void { 
+    this.selectedFile = event.target.files[0]; 
+  }
+  copyImage() {
+   debugger
+    if (!this.selectedFile) { 
+     return;
+    }
+    this.copiedFile(this.selectedFile, '../../../assets/savedImages/image.jpg')
+    
+ }
+ copiedFile(file: File , destinationPath : string) : void {
+  console.log("Filed copy to destination", file,destinationPath)
+ }
+  closeDialog() {
+
+  }
+
+  constructor(private http: HttpClient) {
+    this.employeeForm = new FormGroup({
+
+      avatarUrl: new FormControl('', [Validators.required]),
+      firstName: new FormControl('', [Validators.required, Validators.maxLength(12)]),
+      lastName: new FormControl('', [Validators.required, Validators.maxLength(12)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]),
+      departmentId: new FormControl(parseInt, [Validators.required]),
+      dob: new FormControl('', [
+        Validators.required, Validators.pattern(/^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)$/
+        )
+      ]),
+      skillIds: new FormControl('', Validators.required),
+      shiftIds: new FormControl('', Validators.required,),
+      isActive: new FormControl('', Validators.required)
+
+    })
+
+  }
+  isActive: boolean = false
   handleDateChange(event: any) {
     const selectedDate = event.target.value;
     console.log('Selected date:', selectedDate);
-    
+
     // Handle the selected date as needed
   }
-  SaveChanges(){
-   
-   // this.isValid = this.siginForm.invalid;
+  SaveChanges() {
+
+    // this.isValid = this.siginForm.invalid;
     //debugger;
     const obj = this.employeeForm.value;
     console.log(obj)
     debugger;
-   /* if (this.isValid == false) {
-      this.route.navigate(['dashboard']);
-    }*/
-    this.http.post('https://localhost:7071/User/Add',this.employeeForm.value).subscribe((res: any) => {
+    /* if (this.isValid == false) {
+       this.route.navigate(['dashboard']);
+     }*/
+    this.http.post('https://localhost:7071/User/Add', this.employeeForm.value).subscribe((res: any) => {
       alert("Added Successfully")
-     
- 
-     })
-    
-    }
+   
+    })
+
   }
+}
