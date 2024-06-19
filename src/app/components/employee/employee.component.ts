113752059af732a9827abc6ebc325aebe5f73206
Urvashi Thacker <urvashi.thacker@radixweb.local>
2024-06-19 19:42:52 +0530

    CSV issue resolved

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -26,14 +26,14 @@ import { MatSortModule } from '@angular/material/sort';
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [MatSortModule,MatCardModule, MatTableModule, MatPaginator, MatPaginatorModule, MatButtonModule, MatDialogModule, MatInputModule, MatFormFieldModule, MatRadioModule, MatIconModule, MatDatepickerModule,
+  imports: [MatSortModule, MatCardModule, MatTableModule, MatPaginator, MatPaginatorModule, MatButtonModule, MatDialogModule, MatInputModule, MatFormFieldModule, MatRadioModule, MatIconModule, MatDatepickerModule,
     MatSelectModule, RouterLink, RouterLinkActive, ReactiveFormsModule, HttpClientModule, CommonModule, FileUploadModule],
   templateUrl: './employee.component.html',
 
   styleUrl: './employee.component.css'
 
 })
-export class EmployeeComponent implements OnInit, AfterViewInit {
+export class EmployeeComponent implements OnInit {
   departmentData: { [key: string]: string } = {};
   displayedColumns: string[] = ['firstName', 'lastName', 'gender', 'dob', 'email', 'departmentId', 'skill', 'shift', 'image', 'activityStatus', 'action'];
   userArray: any[] = [];
@@ -41,13 +41,13 @@ export class EmployeeComponent implements OnInit, AfterViewInit {
   users: any[] = []
   skills: any[] = [];
   workingShifts: any[] = []
-  dataSource!: MatTableDataSource<any> 
+  dataSource!: MatTableDataSource<any>
   currentPage: number = 0;
   pageSize: number = 10;
   totalItems!: number;
-  data: any[]=[];
+  data: any[] = [];
   sortColumn!: string;
-  sortDirection!:string;
+  sortDirection!: string;
 
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
@@ -55,37 +55,37 @@ export class EmployeeComponent implements OnInit, AfterViewInit {
 
 
   accountService = inject(AccountService);
-searchTerm: string="";
+  searchTerm: string = "";
   totalPages: any;
   firstIndex: any;
 
- 
+
 
 
   constructor(private route: Router, public dialog: MatDialog, private http: HttpClient, private toastr: ToastrService, private router: Router, private exportService: ExportService) {
   }
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-   
-  }
- 
+  // ngAfterViewInit() {
+  //   this.dataSource.sort = this.sort;
+
+  // }
+
   ngOnInit() {
     this.GetAll()
     this.GetDepartment()
     this.GetSkills()
     this.GetWorkingShifts()
-    this.sortColumn = 'firstName'; // Default sort column
-    this.sortDirection = 'asc'; // Default sort direction
-    
-    
-    }
-    
+    this.sortColumn = 'firstName'; 
+    this.sortDirection = 'asc'; 
+
+
+  }
+
   openDialog(event: Event) {
     debugger
     event.preventDefault();
-  
+
     const dialogRef = this.dialog.open(AddComponent, {
-      
+
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -97,14 +97,14 @@ searchTerm: string="";
       debugger
       const obj = res
       this.users = res
-      
-      
+
+
       this.userArray = this.users.map(user => {
-        
+
         const departmentnm = this.department.find(dp => dp.id === user.departmentId);
         debugger
         const modified = {
-          
+
           id: user.id,
           firstName: user.firstName,
           lastName: user.lastName,
@@ -119,13 +119,14 @@ searchTerm: string="";
           avatarUrl: user.avatarUrl,
           isActive: user.isActive,
           departmentName: departmentnm ? departmentnm.name : 'unkown'
-          }
-          return modified
-          
-          })
-          this.dataSource = new MatTableDataSource<any[]>(this.userArray)
-          this.dataSource.paginator = this.paginator
-          })
+        }
+        return modified
+
+      })
+      this.dataSource = new MatTableDataSource<any[]>(this.userArray)
+      this.dataSource.paginator = this.paginator
+      this.dataSource.sort =this.sort
+    })
   }
 
 
@@ -213,162 +214,166 @@ searchTerm: string="";
 
   exportToCSV() {
     debugger
-    this.exportService.exportToCSV(this.userArray, 'Employee.csv')
+    const exportData = this.userArray.map(employee => {
+      const dob = new Date(employee.dob).toLocaleDateString()
+      return {
+        firstName: employee.firstName,
+        lastName: employee.lastName,
+        gender: this.getGenderLabel(employee.gender),
+        dob: dob,
+        departmentName: this.department.find(dp => dp.id === employee.departmentId)?.name || 'unknown',
+        email: employee.email,
+        userSkillNames: employee.userSkillNames,
+        userWorkingShiftNames: employee.userWorkingShiftNames,
+        isActive: employee.isActive,
+        avatarUrl: employee.avatarUrl
+      };
+    });
+    this.exportService.exportToCSV(exportData, 'Employee.csv')
   }
   exportToExcel() {
     debugger
     this.exportService.exportToExcel(this.userArray, 'Employee.xlsx')
- 
+
+  }
+
+  FilterData(data : Event){
+    const value = (data.target as HTMLInputElement).value
+    this.dataSource.filter = value
+  }
+
+  // fetchDataBySearchTerm(event: Event): void {
+  //   const searchTerm = (event.target as HTMLInputElement).value;
+  //   const apiUrl = `https://localhost:7071/User/GetUsersForFSP?searchTerm=${searchTerm}`;
+  //   this.http.get<any[]>(apiUrl).subscribe((response: any[]) => {
+  //     this.dataSource.data = response.map(user => this.modifyUser(user));
+  //     this.updateUserArrayAndPagination(response.length);
+  //   });
+  // }
+
+  // updateUserArrayAndPagination(totalItems: number): void {
+  //   this.http.get("https://localhost:7071/User/GetAll").subscribe((res: any) => {
+  //     const usersFromAPI = res;
+  //     const modifiedUsers = usersFromAPI.map((user: any) => this.modifyUser(user));
+  //     this.userArray = this.dataSource.data.map(dataSourceUser => {
+  //       const matchedUser = modifiedUsers.find((modifiedUser: { id: any; }) => modifiedUser.id === dataSourceUser.id);
+  //       return matchedUser ? matchedUser : dataSourceUser;
+  //     });
+
+  //     debugger
+  //     this.totalItems = totalItems;
+  //     this.totalPages = Math.ceil(totalItems / this.pageSize);
+  //   });
+  // }
+
+
+
+  // fetchPaginatedData(page: number, pageSize: number): Observable<any[]> {
+  //   const apiUrl = `https://localhost:7071/User/GetUsersForFSP?page=${page}&limit=${pageSize}`;
+  //   return this.http.get<any[]>(apiUrl);
+
+  // }
+
+
+  modifyUser(user: any): any {
+
+    return {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      gender: user.gender,
+      dob: user.dob,
+      email: user.email,
+      departmentId: user.departmentId,
+      skillIds: user.userSkills,
+      shiftIds: user.userWorkingShifts,
+      userSkillNames: user.userSkillNames,
+      userWorkingShiftNames: user.userWorkingShiftNames,
+      avatarUrl: user.avatarUrl,
+      isActive: user.isActive,
+      departmentName: this.department.find(dp => dp.id === user.departmentId)?.name || 'unknown'
+    };
   }
 
 
+  // fetchSortedData(sortColumn: string, sortDirection: string): Observable<any[]> {
+  //   let params = new HttpParams();
+  //   params = params.append('sortColumn', sortColumn);
+  //   params = params.append('sortDirection', sortDirection);
+  //   return this.http.get<any[]>('https://localhost:7071/User/GetUsersForFSP', { params: params });
+  // }
+  // SortData(): void {
+  //   debugger;
+  //   this.fetchSortedData(this.sortColumn, this.sortDirection).subscribe(response => {
+  //     debugger;
 
-  fetchDataBySearchTerm(event: Event): void {
-    const searchTerm = (event.target as HTMLInputElement).value;
-    const apiUrl = `https://localhost:7071/User/GetUsersForFSP?searchTerm=${searchTerm}`;
-    this.http.get<any[]>(apiUrl).subscribe((response: any[]) => {
-      this.dataSource.data = response.map(user => this.modifyUser(user));
-      this.updateUserArrayAndPagination(response.length);
-    });
-  }
-  
-  updateUserArrayAndPagination(totalItems: number): void {
-    this.http.get("https://localhost:7071/User/GetAll").subscribe((res: any) => {
-      const usersFromAPI = res;
-      const modifiedUsers = usersFromAPI.map((user: any) => this.modifyUser(user));
-      this.userArray = this.dataSource.data.map(dataSourceUser => {
-        const matchedUser = modifiedUsers.find((modifiedUser: { id: any; }) => modifiedUser.id === dataSourceUser.id);
-        return matchedUser ? matchedUser : dataSourceUser;
-      });
-      // Update pagination
-      debugger
-      this.totalItems = totalItems;
-      this.totalPages = Math.ceil(totalItems / this.pageSize);
-    });
-  }
-  
+  //     this.dataSource.data = response;
+  //     this.totalItems = response.length;
+  //     this.http.get("https://localhost:7071/User/GetAll").subscribe((res: any) => {
+  //       const usersFromAPI = res;
+  //       const modifiedUsers = usersFromAPI.map((user: { departmentId: any; id: any; firstName: any; lastName: any; gender: any; dob: any; email: any; userSkills: any; userWorkingShifts: any; userSkillNames: any; userWorkingShiftNames: any; avatarUrl: any; isActive: any; }) => {
+  //         const departmentnm = this.department.find(dp => dp.id === user.departmentId);
+  //         debugger
+  //         const modifiedUser = {
+  //           id: user.id,
+  //           firstName: user.firstName,
+  //           lastName: user.lastName,
+  //           gender: user.gender,
+  //           dob: user.dob,
+  //           email: user.email,
+  //           departmentId: user.departmentId,
+  //           skillIds: user.userSkills,
+  //           shiftIds: user.userWorkingShifts,
+  //           userSkillNames: user.userSkillNames,
+  //           userWorkingShiftNames: user.userWorkingShiftNames,
+  //           avatarUrl: user.avatarUrl,
+  //           isActive: user.isActive,
+  //           departmentName: departmentnm ? departmentnm.name : 'unknown'
+  //         };
+  //         return modifiedUser;
+  //       });
+  //       debugger
+  //       const mappedUsers = this.dataSource.data.map(dataSourceUser => {
+  //         const matchedUser = modifiedUsers.find((modifiedUser: { id: any; }) => modifiedUser.id === dataSourceUser.id);
+  //         if (matchedUser) {
+  //           return matchedUser;
+  //         } else {
+  //           return dataSourceUser;
+  //         }
+  //       });
+  //       this.userArray = mappedUsers;
+  //     });
+  //   });
+  // }
 
-        
-fetchPaginatedData(page: number, pageSize: number): Observable<any[]> {
-  const apiUrl = `https://localhost:7071/User/GetUsersForFSP?page=${page}&limit=${pageSize}`;
-  return this.http.get<any[]>(apiUrl);
-  
-}
-
-
-modifyUser(user: any): any {
-  // Modify user data here as per your requirements
-  return {
-    id: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    gender: user.gender,
-    dob: user.dob,
-    email: user.email,
-    departmentId: user.departmentId,
-    skillIds: user.userSkills,
-    shiftIds: user.userWorkingShifts,
-    userSkillNames: user.userSkillNames,
-    userWorkingShiftNames: user.userWorkingShiftNames,
-    avatarUrl: user.avatarUrl,
-    isActive: user.isActive,
-    departmentName: this.department.find(dp => dp.id === user.departmentId)?.name || 'unknown'
-  };
-}
-
-
-fetchSortedData(sortColumn: string, sortDirection: string): Observable<any[]> {
-  let params = new HttpParams();
-      params = params.append('sortColumn', sortColumn);
-      params = params.append('sortDirection', sortDirection);
-      return this.http.get<any[]>('https://localhost:7071/User/GetUsersForFSP', { params: params });
-}
-SortData(): void {
-  debugger;
-  this.fetchSortedData(this.sortColumn, this.sortDirection).subscribe(response => {
-    debugger;
-
-    this.dataSource.data = response;
-
- 
-    this.totalItems = response.length;
+  // applySorting(column: string) {
+  //   this.SortData()
+  //   if (this.sortColumn === column) {
+  //     this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+  //   } else {
+  //     this.sortColumn = column;
+  //     this.sortDirection = 'asc';
+  //   }
 
 
-    this.http.get("https://localhost:7071/User/GetAll").subscribe((res: any) => {
-    
-      const usersFromAPI = res;
+  //   this.userArray.sort((a, b) => {
+  //     const valA = a[column];
+  //     const valB = b[column];
+  //     if (valA < valB) {
+  //       return this.sortDirection === 'asc' ? -1 : 1;
+  //     } else if (valA > valB) {
+  //       return this.sortDirection === 'asc' ? 1 : -1;
+  //     } else {
+  //       return 0;
+  //     }
+  //   });
 
-      
-      const modifiedUsers = usersFromAPI.map((user: { departmentId: any; id: any; firstName: any; lastName: any; gender: any; dob: any; email: any; userSkills: any; userWorkingShifts: any; userSkillNames: any; userWorkingShiftNames: any; avatarUrl: any; isActive: any; }) => {
-        const departmentnm = this.department.find(dp => dp.id === user.departmentId);
- debugger
-     
-        const modifiedUser = {
-          id: user.id,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          gender: user.gender,
-          dob: user.dob,
-          email: user.email,
-          departmentId: user.departmentId,
-          skillIds: user.userSkills,
-          shiftIds: user.userWorkingShifts,
-          userSkillNames: user.userSkillNames,
-          userWorkingShiftNames: user.userWorkingShiftNames,
-          avatarUrl: user.avatarUrl,
-          isActive: user.isActive,
-          departmentName: departmentnm ? departmentnm.name : 'unknown'
-        };
-        return modifiedUser;
-      });
-debugger
-    
-      const mappedUsers = this.dataSource.data.map(dataSourceUser => {
-        
-        const matchedUser = modifiedUsers.find((modifiedUser: { id: any; }) => modifiedUser.id === dataSourceUser.id);
-        if (matchedUser) {
-          return matchedUser;
-        } else {
-          return dataSourceUser;
-        }
-      });
-        this.userArray = mappedUsers;
-    });
-  });
-}
-
-applySorting(column: string) {
-  this.SortData()
-      if (this.sortColumn === column) {
-        // If the same column is clicked, toggle the sort direction
-        this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
-      } else {
-        // If a new column is clicked, set the sort column and default to ascending direction
-        this.sortColumn = column;
-        this.sortDirection = 'asc';
-      }
-    
-      // Sort the userArray based on the selected column and direction
-      this.userArray.sort((a, b) => {
-        const valA = a[column];
-        const valB = b[column];
-        if (valA < valB) {
-          return this.sortDirection === 'asc' ? -1 : 1;
-        } else if (valA > valB) {
-          return this.sortDirection === 'asc' ? 1 : -1;
-        } else {
-          return 0;
-        }
-      });
-
-  this.fetchSortedData(this.sortColumn, this.sortDirection).subscribe(response => {
-    this.dataSource.data = response;
-    this.dataSource.sort = this.sort;
-    // this.totalItems = response.length; // Update totalItems if needed
-    // this.paginator.length = this.totalItems;
-  });
-}
+  //   this.fetchSortedData(this.sortColumn, this.sortDirection).subscribe(response => {
+  //     this.dataSource.data = response;
+  //     this.dataSource.sort = this.sort;
+  //   });
+  // }
 
 }
 
-  
+
